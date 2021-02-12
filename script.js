9b4577b7416c15299e3d42e26712ded5c7c741f6
prokeratin71 <78900053+prokeratin71@users.noreply.github.com>
2021-02-12 14:25:50 +0300

    Create structure. Step 1

---
+++ script.js
@@ -0,0 +1,8 @@
+$(".nav li a").on("click", function() {
+  var activeTab = $(this).attr("href");
+  $('#card > div').removeClass('flipped after before');
+  $(activeTab).addClass('flipped');
+  $(activeTab).prevAll('.flipper').addClass('before');
+  $(activeTab).nextAll('.flipper').addClass('after');
+  return false;
+});
